e9fd77e1458f8d3d8c26ebbe9e1b7ff8f0720f0a
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-06-09 00:19:42 +0200

    Implement CRUD operations for posts in the router

--- routers/posts.js
+++ routers/posts.js
@@ -28,7 +28,39 @@ router.get('/:id', (req, res) => {
     res.json(post);
 });
 
+// STORE
+router.post('/', (req, res) => {
+    const { title, content, tags } = req.body;
+    const newPost = {
+        id: posts.length + 1,
+        title,
+        content,
+        tags
+    };
+    posts.push(newPost);
+    res.status(201).json(newPost);
+});
 
+// UPDATE
+router.put('/:id', (req, res) => { });
+
+// PATCH
+router.patch('/:id', (req, res) => { });
+
+// DELETE
+router.delete('/:id', (req, res) => {
+
+    const postId = parseInt(req.params.id);
+    const thisPost = posts.find(post => post.id === postId);
+
+    if (!thisPost) {
+        return res.status(404).json({ error: true, message: 'Post not found' });
+    }
+
+    const postIndex = posts.indexOf(thisPost);
+    posts.splice(postIndex, 1);
+    res.json({ message: 'Post deleted successfully' });
+});
 
 
 module.exports = router;
